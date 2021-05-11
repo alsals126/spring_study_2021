92a161960a3b9814ef8281a1a1d3b2f855c687ba
오정민 <9970mia@naver.com>
2021-05-11 13:25:41 +0900

    210511

--- dev/DI210401/src/main/java/kr/hs/study/beans/AppleSpeaker.java
+++ dev/DI210401/src/main/java/kr/hs/study/beans/AppleSpeaker.java
@@ -1,7 +1,11 @@
 package kr.hs.study.beans;
 
 public class AppleSpeaker implements Speaker {
-
+	
+	public void AppleSpeaker() {
+		System.out.println("applespeaker 입니다.");
+	}
+	
 	public void volumeUP() {
 		// TODO Auto-generated method stub
 		System.out.println("applespeaker UP");
--- dev/DI210401/src/main/java/kr/hs/study/beans/SongSpeaker.java
+++ dev/DI210401/src/main/java/kr/hs/study/beans/SongSpeaker.java
@@ -1,7 +1,9 @@
 package kr.hs.study.beans;
 
 public class SongSpeaker implements Speaker {
-
+	public SongSpeaker() {
+		System.out.println("songspeaker 입니다.");
+	}
 	public void volumeUP() {
 		// TODO Auto-generated method stub
 		System.out.println("songspeaker UP");
